8d77b7645e5eeff8c1c319167c0e50ffd5db5550
Ridho144 <ridho23ti@mahasiswa.pcr.ac.id>
2025-05-24 20:29:57 +0700

    Melakukan Perubahan pada file ini

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
-
+//Penambahan Komen
 root.render(
   <BrowserRouter>
     <App />
